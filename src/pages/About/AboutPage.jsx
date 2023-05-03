330bc51517d07e7f29957992ecabf9b01c61b9c6
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-05-03 21:59:59 +0300

    _blank

--- src/pages/About/AboutPage.jsx
+++ src/pages/About/AboutPage.jsx
@@ -90,12 +90,12 @@ const AboutPage = () => {
                     <AboutSubTitle># Information</AboutSubTitle>
                     <AboutList>
                         <li>Havryliuk Oleksand</li>
-                        <li><a href="mailto:gavrfavorite@gmail.com">
+                        <li><a target="_blank" rel="noreferrer" href="mailto:gavrfavorite@gmail.com">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-envelop`}></use>
                             </svg>gavrfavorite@gmail.com</a>
                         </li>
-                        <li><a href="https://github.com/AlekasndrHavryliuk">
+                        <li><a target="_blank" rel="noreferrer" href="https://github.com/AlekasndrHavryliuk">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-github`}></use>
                             </svg>AlekasndrHavryliuk</a>
@@ -106,22 +106,22 @@ const AboutPage = () => {
                 <div>
                     <AboutSubTitle># Social Links</AboutSubTitle>
                     <AboutList>
-                        <li><a href="https://t.me/algavriluk">
+                        <li><a target="_blank" rel="noreferrer" href="https://t.me/algavriluk">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-telegram`}></use>
                             </svg>@algavriluk</a>
                         </li>
-                        <li><a href="https://www.reddit.com/user/HavryliukAl">
+                        <li><a target="_blank" rel="noreferrer" href="https://www.reddit.com/user/HavryliukAl">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-reddit`}></use>
                             </svg>HavryliukAl</a>
                         </li>
-                        <li><a href="https://instagram.com/al_gavrilyuk?igshid=YmMyMTA2M2Y=">
+                        <li><a target="_blank" rel="noreferrer" href="https://instagram.com/al_gavrilyuk?igshid=YmMyMTA2M2Y=">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-instagram`}></use>
                             </svg>al_gavrilyuk</a>
                         </li>
-                        <li><a href="https://www.linkedin.com/in/aleksandr-havryliuk/">
+                        <li><a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/aleksandr-havryliuk/">
                             <svg width={24} height={24}>
                                 <use href={`${svg}#icon-linkedin`}></use>
                             </svg>Aleksandr Havryliuk</a>

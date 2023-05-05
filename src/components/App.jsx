60131ebf27a350d98e7e27634ca54e9eec871462
AlekasndrHavryliuk <104818533+AlekasndrHavryliuk@users.noreply.github.com>
2023-05-05 21:01:55 +0300

    Env

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -39,9 +39,9 @@ export const App = () => {
     }
   }, [authComplete, user?.token, dispatch])
 
-  console.log(process.env.REACT_APP_BASE_URL);
+  console.log(process.env.REACT_APP_MY_API_KEY);
   // console.log(process.env);
-  console.log(process.env.REACT_APP_API_KEY);
+  console.log(process.env.REACT_APP_VORTEX_URL);
 
   return (
     <ThemeProvider theme={darkMode ? darkThemeVars : lightThemeVars}>
